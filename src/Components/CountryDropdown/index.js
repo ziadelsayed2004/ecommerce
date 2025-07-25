992059217c745f9e52491f4c2704416939ca55f1
ziadelsayed2004 <ziadelsayed2004@gmail.com>
2025-07-25 22:33:41 +0300

    V1

--- src/Components/CountryDropdown/index.js
+++ src/Components/CountryDropdown/index.js
@@ -1,13 +1,25 @@
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
+import React from 'react';
 
 import { FaAngleDown } from "react-icons/fa6";
 import { IoSearch } from "react-icons/io5";
 
-const countryDropdown = ()=>{
+const CountryDropdown = ()=>{
+    
+    const [open, setOpen] = React.useState(false);
+
+      const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <>
-            <Button className='countryDrop'>
+            <Button className='countryDrop'  onClick={handleClickOpen}>
                 <div className='info d-flex flex-column'>
                     <span className='label'>Your Location</span>
                     <span className='name'>Egypt</span>
@@ -15,7 +27,7 @@ const countryDropdown = ()=>{
                 <span className='ml-auto'><FaAngleDown /></span>
             </Button>
 
-            <Dialog className='locationModel' open={true}>
+            <Dialog className='locationModel' open={open} onClose={handleClose}>
                 <h4>Choose Your Location</h4>
                 <p>Enter Your Location To Get Your Offers</p>
                 <div className='headerSearch ml-3 mr-3'>
@@ -27,4 +39,4 @@ const countryDropdown = ()=>{
     )
 }
 
-export default countryDropdown;
+export default CountryDropdown;
