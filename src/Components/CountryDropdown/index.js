32ee3c8c83b7da78bf603e8ecabe9d807bef2b6c
ziadelsayed2004 <ziadelsayed2004@gmail.com>
2025-07-26 00:24:24 +0300

    V1

--- src/Components/CountryDropdown/index.js
+++ src/Components/CountryDropdown/index.js
@@ -4,22 +4,15 @@ import React from 'react';
 
 import { FaAngleDown } from "react-icons/fa6";
 import { IoSearch } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 
 const CountryDropdown = ()=>{
-
-    const [open, setOpen] = React.useState(false);
-
-      const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
+    
+    const [isOpenModel, setIsOpenModel] = React.useState(false);
 
     return (
         <>
-            <Button className='countryDrop'  onClick={handleClickOpen}>
+            <Button className='countryDrop'  onClick={()=>setIsOpenModel(true)}>
                 <div className='info d-flex flex-column'>
                     <span className='label'>Your Location</span>
                     <span className='name'>Egypt</span>
@@ -27,9 +20,10 @@ const CountryDropdown = ()=>{
                 <span className='ml-auto'><FaAngleDown /></span>
             </Button>
 
-            <Dialog className='locationModel' open={open} onClose={handleClose}>
+            <Dialog className='locationModel' onClose={()=>setIsOpenModel(false)} open={isOpenModel}>
                 <h4>Choose Your Location</h4>
                 <p>Enter Your Location To Get Your Offers</p>
+                <Button className='close_' onClick={()=>setIsOpenModel(false)}><IoClose /></Button>
                 <div className='headerSearch ml-3 mr-3'>
                     <input type='text' placeholder='Search Your Area...' />
                     <Button><IoSearch /></Button>
