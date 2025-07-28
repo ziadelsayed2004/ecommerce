db47a9568e11dcd9965d862f169c8c1a1c33920b
ziadelsayed2004 <ziadelsayed2004@gmail.com>
2025-07-28 15:59:41 +0300

    V1

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,34 @@
+import Button from '@mui/material/Button';
 import HomeBanner from "../../Components/HomeBanner";
+import ProductImg from "../../assets/images/ProductBanner.png";
+
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const Home = () =>{
     return(
         <>
             <HomeBanner />
+
+            <section className="homeProducts">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-3">
+                            <div className="banner">
+                                <img src={ProductImg} className="cursor-pointer" alt="ProductBanner" />
+                            </div>
+                        </div>
+                        <div className="col-md-9 ProductRow">
+                            <div className="d-flex align-items-center BestProducts">
+                                <div className="info w-75">
+                                    <h3>Best Seller</h3>
+                                    <p>Do not miss the current offers and sales !</p>
+                                </div>
+                                <Button className='ViewAllBtn ml-auto'>View All<FaArrowRightLong /></Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
