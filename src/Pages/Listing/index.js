6a8792231a9e81f2fb36607f41542b2639296008
ziadelsayed2004 <ziadelsayed2004@gmail.com>
2025-08-08 16:36:34 +0300

    V1

--- src/Pages/Listing/index.js
+++ src/Pages/Listing/index.js
@@ -1,15 +1,32 @@
+import React from "react";
+import Button from "@mui/material/Button";
 import Sidebar from "../../Components/Sidebar";
 
+import { IoClose } from "react-icons/io5";
+import { IoIosMenu } from "react-icons/io";
+
+
+
 const Listing = () =>{
+    const [sidebarOpen, setSidebarOpen] = React.useState(false);
     return(
         <>
             <section className="productListingPage">
                 <div className="container">
                     <div className="productList d-flex">
-                        <Sidebar />
+
+                        <div className={`sidebarWrapper ${sidebarOpen ? "open" : ""}`}>
+                            <Button className='close_ circle' onClick={() => setSidebarOpen(false)}><IoClose /></Button>
+                            <Sidebar />
+                        </div>
+                        {sidebarOpen && (<div className="overlay" onClick={() => setSidebarOpen(false)}></div> )}
 
                         <div className="rightContent">
-                            
+                            <Button className="toggleSidebarBtn d-md-none" onClick={() => setSidebarOpen(true)}>
+                                <span className='icon'><IoIosMenu/></span>
+                                <span className='text'>Filter</span>
+                            </Button>
+
                         </div>
                     </div>
                 </div>
